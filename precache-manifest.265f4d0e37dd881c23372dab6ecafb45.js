b87551a601f6a124dfbf37b2e186d246721b39be
chunkitmax <chunkitmax1125@gmail.com>
2019-07-16 19:17:58 +0800

    Updates

---
+++ precache-manifest.265f4d0e37dd881c23372dab6ecafb45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ab763744366c034b2189d18b5ac669",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "481f9f89103d84e048cd",
+    "url": "/about/static/css/main.199b77e1.chunk.css"
+  },
+  {
+    "revision": "5994be439e5eaa4df610",
+    "url": "/about/static/js/2.deb3055c.chunk.js"
+  },
+  {
+    "revision": "481f9f89103d84e048cd",
+    "url": "/about/static/js/main.dbaf0dd9.chunk.js"
+  },
+  {
+    "revision": "5e7b65462749e51da51f",
+    "url": "/about/static/js/runtime~main.c0e869d3.js"
+  }
+]);
